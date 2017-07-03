a79535ff833e7575a15917943c773de66ab58a9a
alok <alok>
2017-07-03 20:26:41 +0000

    initial commit

---
+++ AddNewSlaveClient.groovy
@@ -0,0 +1,45 @@
+job('ADD-new-slave-dsl') {
+    parameters {
+        stringParam('JENKINS_URL', 'http://192.168.33.99:8080')
+        stringParam('REMOTE_MACHINE_IP')
+        stringParam('CRED_ID')
+        stringParam('jenkins_username')
+        stringParam('jenkins_password', 'jenkins_password')
+        stringParam('remote_machine_username')
+        stringParam('LABEL')
+    }
+  
+  steps {
+        shell(
+
+        '#!/bin/bash\n'  +
+        'rm -rf ${WORKSPACE}/*.groovy\n' +
+		'cp ${JENKINS_HOME}/scripts/DeployNginxdsl.template ${WORKSPACE}/DeployNginxdsl.groovy\n' +
+		'cp ${JENKINS_HOME}/scripts/DeployTomcatdsl.template ${WORKSPACE}/DeployTomcatdsl.groovy\n' +
+		'cp ${JENKINS_HOME}/scripts/DeployMysqldsl.template ${WORKSPACE}/DeployMysqldsl.groovy\n' +
+		'cp ${JENKINS_HOME}/scripts/nested.template ${WORKSPACE}/nested.groovy\n' +
+		'sed -i "s/{{LABEL}}/${LABEL}/g" DeployNginxdsl.groovy\n' +
+		'sed -i "s/{{LABEL}}/${LABEL}/g" DeployTomcatdsl.groovy\n' +
+		'sed -i "s/{{LABEL}}/${LABEL}/g" DeployMysqldsl.groovy\n' +
+		'sed -i "s/{{LABEL}}/${LABEL}/g" nested.groovy\n' 
+        )
+    }
+  
+  
+       steps {
+        dsl {
+            external('*.groovy')
+            
+        }
+    }
+      
+  
+    steps {
+        shell(
+
+        '#!/bin/bash\n'  +
+          '/var/lib/jenkins/scripts/node.sh ${JENKINS_URL} ${REMOTE_MACHINE_IP} ${CRED_ID} ${jenkins_username} ${jenkins_password} ${remote_machine_username} &{LABEL}\n'
+
+        )
+    }
+}
---
+++ node.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+JENKINS_URL=$1
+NODE_NAME=$2
+NODE_SLAVE_HOME='/home/jenkins'
+EXECUTORS=1
+SSH_PORT=22
+CRED_ID=$3
+RANDOMENO=`shuf -i 1-99 -n 1`
+#LABELS=slave-${RANDOMENO}
+USERID=${USER}
+jenkins_username=$4
+jenkins_password=$5
+remote_machine_username=$6
+LABELS=$7
+
+scp -o StrictHostKeyChecking=no /var/lib/jenkins/jdk-8u131-linux-x64.rpm ${remote_machine_username}@${NODE_NAME}:${NODE_SLAVE_HOME}/.
+ssh -o StrictHostKeyChecking=no ${remote_machine_username}@${NODE_NAME} "mkdir /home/jenkins"
+ssh -o StrictHostKeyChecking=no ${remote_machine_username}@${NODE_NAME} "sudo yum install epel-release"
+ssh -o StrictHostKeyChecking=no ${remote_machine_username}@${NODE_NAME} "sudo yum install ansible"
+
+ssh -o StrictHostKeyChecking=no ${remote_machine_username}@${NODE_NAME} "sudo yum -y install jdk-8u131-linux-x64.rpm"
+
+cat <<EOF | java -jar ~/jenkins-cli.jar -s $1 create-node $2 --username "$4" --password "$5"
+<slave>
+  <name>${NODE_NAME}</name>
+  <description></description>
+  <remoteFS>${NODE_SLAVE_HOME}</remoteFS>
+  <numExecutors>${EXECUTORS}</numExecutors>
+  <mode>NORMAL</mode>
+  <retentionStrategy class="hudson.slaves.RetentionStrategy$Always"/>
+  <launcher class="hudson.plugins.sshslaves.SSHLauncher" plugin="ssh-slaves@1.5">
+    <host>${NODE_NAME}</host>
+    <port>${SSH_PORT}</port>
+    <credentialsId>${CRED_ID}</credentialsId>
+  </launcher>
+  <label>${LABELS}</label>
+  <nodeProperties/>
+  <userId>${USERID}</userId>
+</slave>
+EOF
