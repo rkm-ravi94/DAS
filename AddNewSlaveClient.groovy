d4dac126943b93505d197473cfc9be6df44fa113
alok <alok>
2017-07-04 06:36:18 +0000

    AddNewSlaveClient.groovy

--- AddNewSlaveClient.groovy
+++ AddNewSlaveClient.groovy
@@ -4,7 +4,9 @@ job('ADD-new-slave-dsl') {
         stringParam('REMOTE_MACHINE_IP', '192.168.33.99')
         stringParam('CRED_ID', '7a8b1045-cc12-4f79-a966-f047789bcf18')
         stringParam('jenkins_username', 'alok')
-        password('jenkins_password')
+        password{
+		('jenkins_password')
+	}
         stringParam('remote_machine_username', 'root')
         stringParam('LABEL', '')
     }
