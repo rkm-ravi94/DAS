2f59a909fbcebcaa21f1e94c859885e64b907000
alok <alok>
2017-07-04 18:22:13 +0000

    jenkins.properties

--- CreateJenkinsPropertiesFile.groovy
+++ CreateJenkinsPropertiesFile.groovy
@@ -10,7 +10,7 @@ job('Create-Jenkins-Properties-File') {
         shell(
 
         '#!/bin/bash\n'  +
-		'sed -i "s/{{JENKINS_URL}}/${JENKINS_URL}/g" /var/lib/jenkins/properties/jenkins.properties\n' +
+		'sed -i "s/{{IP}}/${IP}/g" /var/lib/jenkins/properties/jenkins.properties\n' +
 		'sed -i "s/{{USERNAME}}/${USERNAME}/g" /var/lib/jenkins/properties/jenkins.properties\n' +
 		'sed -i "s/{{PASSWORD}}/${PASSWORD}/g" /var/lib/jenkins/properties/jenkins.properties\n' +
 		'sed -i "s/{{CRED_ID}}/${CRED_ID}/g" /var/lib/jenkins/properties/jenkins.properties\n'
