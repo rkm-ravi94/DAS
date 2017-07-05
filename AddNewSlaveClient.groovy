dbf2e2323e2c5ffcd5936e8989a6280cc27d073f
alok <alok>
2017-07-05 07:47:36 +0000

    AddNewSlaveClient.groovy

--- AddNewSlaveClient.groovy
+++ AddNewSlaveClient.groovy
@@ -24,7 +24,7 @@ job('Add-Node') {
 		'sed -i "s/{{LABEL}}/${LABEL}/g" Deployzabbixdsl.groovy\n' +
 		'sed -i "s/{{LABEL}}/${LABEL}/g" nested.groovy\n' + 
 		'cp Inventory_File ${JENKINS_HOME}/inventory/hosts\n' +
-	#	'cat ${JENKINS_HOME}/inventory/hosts  | grep "\\[" | sed "s/.//;s/.$//" | paste -s -d ',' > ${JENKINS_HOME}/inventory.Properties\n'
+	//	'cat ${JENKINS_HOME}/inventory/hosts  | grep "\\[" | sed "s/.//;s/.$//" | paste -s -d ',' > ${JENKINS_HOME}/inventory.Properties\n'
         )
     }
   
