befce84a082a33fb86d75a1e222adb9d3130f15f
Your Name <you@example.com>
2017-07-06 16:01:32 +0530

    updated

--- AnsibleCommand.groovy
+++ AnsibleCommand.groovy
@@ -1,15 +1,40 @@
-job('AnsibleCommand') {
+
+job('ansible-command') {
         parameters {
-                stringParam('command')
+     	extendedChoiceParameterDefinition {
+          name('Deployment_Group' )
+          type('Single Select' )
+          propertyFile('/var/lib/jenkins/inventory.Properties')
+          propertyKey('key')
+          defaultValue('targets')
+          defaultPropertyFile('')
+          value('')
+          defaultPropertyKey('') 
+          quoteValue(false)
+          visibleItemCount(5) 
+       	  description('')
+          multiSelectDelimiter('')
+          bindFieldName('')
+          svnPath(false)
+          svnUrl('')
+          svnUserName('')
+          svnPassword('')
+          projectName('')
+          roleBasedFilter(false)
+        }
+          stringParam('Command')
+}
+  
+   label('Facebook')
 
-            }
+steps {
+    shell(
 
-            steps {
-            shell(
+    '#!/bin/bash -x\n'  +
+    'name=$Command\n'  +
+    'export ANSIBLE_HOST_KEY_CHECKING=False\n'  +
+    'ansible ${Deployment_Group} -a  "$name"\n' 
 
-            'name=$Command\n'  +
-            'echo $name\n'
-
-            )
-            }
-            }
+)
+}
+}
