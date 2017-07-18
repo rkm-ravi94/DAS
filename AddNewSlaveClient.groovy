1261bdf20f0ae19cafb588d56b0e9b738995dafb
alok <alokin.ntl@gmail.com>
2017-07-18 07:42:30 +0000

    updated

--- AddNewSlaveClient.groovy
+++ AddNewSlaveClient.groovy
@@ -18,12 +18,16 @@ job('Add-Node') {
 		'cp ${JENKINS_HOME}/template/DeployMysqldsl.template ${WORKSPACE}/DeployMysqldsl.groovy\n' +
 		'cp ${JENKINS_HOME}/template/nested.template ${WORKSPACE}/nested.groovy\n' +
 		'cp ${JENKINS_HOME}/template/Deployzabbixdsl.template ${WORKSPACE}/Deployzabbixdsl.groovy\n' +
+		'cp ${JENKINS_HOME}/template/TomcatDeployment.template ${WORKSPACE}/TomcatDeployment.groovy\n' +
+		'cp ${JENKINS_HOME}/template/MysqlReplication.template ${WORKSPACE}/MysqlReplication.groovy\n' +
 		'cp ${JENKINS_HOME}/template/AnsibleCommand.template ${WORKSPACE}/AnsibleCommand.groovy\n' +
 		'sed -i "s/{{LABEL}}/${LABEL}/g" DeployNginxdsl.groovy\n' +
 		'sed -i "s/{{LABEL}}/${LABEL}/g" DeployTomcatdsl.groovy\n' +
 		'sed -i "s/{{LABEL}}/${LABEL}/g" DeployMysqldsl.groovy\n' +
 		'sed -i "s/{{LABEL}}/${LABEL}/g" Deployzabbixdsl.groovy\n' +
-		'sed -i "s/{{LABEL}}/${LABEL}/g" nested.groovy\n' + 
+		'sed -i "s/{{LABEL}}/${LABEL}/g" nested.groovy\n' +
+		'sed -i "s/{{LABEL}}/${LABEL}/g" TomcatDeployment.groovy\n' + 
+		'sed -i "s/{{LABEL}}/${LABEL}/g" MysqlReplication.groovy\n' +
 		'sed -i "s/{{LABEL}}/${LABEL}/g" AnsibleCommand.groovy\n' +
 		
 		'cp Inventory_File ${JENKINS_HOME}/inventory/hosts\n' + 
