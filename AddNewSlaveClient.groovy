b160779c90384412861dab6565a5aff11f5a6280
alok <alokin.ntl@gmail.com>
2017-07-19 14:39:22 +0000

    AddNewSlaveClient.groovy

--- AddNewSlaveClient.groovy
+++ AddNewSlaveClient.groovy
@@ -1,10 +1,10 @@
 job('Add-Node') {
     parameters {
-        stringParam('REMOTE_MACHINE_IP', '192.168.33.99')
+        stringParam('REMOTE_MACHINE_IP', '')
         stringParam('LABEL', '')
 	fileParam{
      		 name('Inventory_File')
-      		description ('select file')
+      		description ('Inventory Host File')
     }
     }
   
@@ -12,6 +12,7 @@ job('Add-Node') {
         shell(
 
         '#!/bin/bash\n'  +
+
         'rm -rf ${WORKSPACE}/*.groovy\n' +
 		'cp ${JENKINS_HOME}/template/DeployNginxdsl.template ${WORKSPACE}/DeployNginxdsl.groovy\n' +
 		'cp ${JENKINS_HOME}/template/DeployTomcatdsl.template ${WORKSPACE}/DeployTomcatdsl.groovy\n' +
@@ -27,11 +28,12 @@ job('Add-Node') {
 		'sed -i "s/{{LABEL}}/${LABEL}/g" Deployzabbixdsl.groovy\n' +
 		'sed -i "s/{{LABEL}}/${LABEL}/g" nested.groovy\n' +
 		'sed -i "s/{{LABEL}}/${LABEL}/g" TomcatDeployment.groovy\n' + 
+
 		'sed -i "s/{{LABEL}}/${LABEL}/g" MysqlReplication.groovy\n' +
-		'sed -i "s/{{LABEL}}/${LABEL}/g" AnsibleCommand.groovy\n' +
-		
+
+		'sed -i "s/{{LABEL}}/${LABEL}/g" AnsibleCommand.groovy\n' +		
 		'cp Inventory_File ${JENKINS_HOME}/inventory/hosts\n' + 
-		'keyValue=`cat ${JENKINS_HOME}/inventory/hosts  | grep "\\[" | sed "s/.//;s/.$//" | paste -s -d ","`\n' +
+		'keyValue=`cat ${JENKINS_HOME}/inventory/hosts  | grep "\\[" | sed "s/.//;s/.$//"| cut -d "]" -f1 | paste -s -d ","`\n' +
 		'echo "key=${keyValue}" > ${JENKINS_HOME}/inventory.Properties\n' +
 		
 		'scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa root@${REMOTE_MACHINE_IP}:~/.ssh/id_rsa'
--- DeleteSlaveClient.groovy
+++ DeleteSlaveClient.groovy
@@ -1,7 +1,6 @@
 job('Delete-Node') {
     parameters {
-        stringParam('NODE_NAME', '192.168.33.19')
-        stringParam('LABEL', '')
+        stringParam('NODE_NAME', '')
     }
   label('master')
   steps {
@@ -9,12 +8,15 @@ job('Delete-Node') {
 
         '#!/bin/bash\n'  +
 		'source ${JENKINS_HOME}/properties/jenkins.properties\n' +
+		'LABEL=`java -jar ${JENKINS_CLI_JAR} -s ${JENKINS_URL} get-node ${NODE_NAME} --username "${USERNAME}" --password "${PASSWORD}" | grep "label"  | cut -d ">" -f2 | cut -d "<" -f1`\n' +
 		'java -jar ${JENKINS_CLI_JAR} -s ${JENKINS_URL} delete-node ${NODE_NAME} --username "${USERNAME}" --password "${PASSWORD}"\n' +
 		'java -jar ${JENKINS_CLI_JAR} -s ${JENKINS_URL} delete-job ${LABEL}-Setup-Nginx-Job --username "${USERNAME}" --password "${PASSWORD}"\n' +
 		'java -jar ${JENKINS_CLI_JAR} -s ${JENKINS_URL} delete-job ${LABEL}-Setup-Mysql-Job --username "${USERNAME}" --password "${PASSWORD}"\n' +
 		'java -jar ${JENKINS_CLI_JAR} -s ${JENKINS_URL} delete-job ${LABEL}-Setup-Tomcat-Job --username "${USERNAME}" --password "${PASSWORD}"\n' +
 		'java -jar ${JENKINS_CLI_JAR} -s ${JENKINS_URL} delete-job ${LABEL}-Setup-Zabbix-Job --username "${USERNAME}" --password "${PASSWORD}"\n' +
 		'java -jar ${JENKINS_CLI_JAR} -s ${JENKINS_URL} delete-job ${LABEL}-Command-Executor --username "${USERNAME}" --password "${PASSWORD}"\n' +
+		'java -jar ${JENKINS_CLI_JAR} -s ${JENKINS_URL} delete-job ${LABEL}-Mysql-Replication --username "${USERNAME}" --password "${PASSWORD}"\n' +
+		'java -jar ${JENKINS_CLI_JAR} -s ${JENKINS_URL} delete-job ${LABEL}-Tomcat-Deployment --username "${USERNAME}" --password "${PASSWORD}"\n' +
 		'java -jar ${JENKINS_CLI_JAR} -s ${JENKINS_URL} delete-view ${LABEL}-jobs --username "${USERNAME}" --password "${PASSWORD}"\n'
 
         )
